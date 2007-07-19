2ecbade99e6cd5e5f728ae245c167e4bcaa33ce6
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-07-19 15:42:24 +0200

    small enhancement of the check.cxx template
    darcs-hash:20070719134224-2fc9d-c73a3eb399b6c536349aa63904ac74f05964c8c4.gz

--- check.cxx
+++ check.cxx
@@ -1,6 +1,5 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkCommand.h"
 #include "itkSimpleFilterWatcher.h"
 
 #include "itkImageFilter.h"
@@ -9,9 +8,11 @@
 int main(int argc, char * argv[])
 {
 
-  if( argc !=  )
+  if( argc != 3 )
     {
-    std::cerr << "usage: " << argv[0] << " " << std::endl;
+    std::cerr << "usage: " << argv[0] << " intput output" << std::endl;
+    std::cerr << " input: the input image" << std::endl;
+    std::cerr << " output: the output image" << std::endl;
     // std::cerr << "  : " << std::endl;
     exit(1);
     }
